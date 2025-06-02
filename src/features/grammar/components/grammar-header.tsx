7ea407f0e67ada3bf8142c020c55561ccdd3a80c
duongductrong <duongductrong06@gmail.com>
2025-06-03 01:16:55 +0700

    chore: remove back button on grammar header

--- src/features/grammar/components/grammar-header.tsx
+++ src/features/grammar/components/grammar-header.tsx
@@ -1,8 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { urls } from "@/constants/urls";
 import { cn } from "@/lib/tw";
-import { ChevronLeftIcon } from "lucide-react";
-import Link from "next/link";
 import { ComponentProps } from "react";
 
 export interface GrammarHeaderProps extends ComponentProps<"h2"> {}
@@ -14,12 +10,6 @@ export const GrammarHeader = ({
 }: GrammarHeaderProps) => {
   return (
     <h2 className={cn("text-title font-bold", className)} {...props}>
-      <Button variant="ghost" size="icon" asChild>
-        <Link href={urls.grammar.index}>
-          <ChevronLeftIcon className="size-4" />
-        </Link>
-      </Button>
-
       {children}
     </h2>
   );
