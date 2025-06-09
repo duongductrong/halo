8c21e04df001611815fbef967fe372b52481d87b
Duong Duc Trong <duongductrong06@gmail.com>
2025-06-09 22:24:10 +0700

    chore: ignore auth temporary

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,25 +1,23 @@
 import AppLayout from "@/components/app-layout";
 import { AppMain } from "@/components/app-main";
 import { AppSidebar } from "@/components/app-sidebar";
-import { createClient } from "@/lib/supabase/server";
-import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
 
 type Props = PropsWithChildren;
 
 const Layout = async ({ children }: Props) => {
-  const supabase = await createClient();
+  // const supabase = await createClient();
 
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser();
+  // const {
+  //   data: { user },
+  //   error,
+  // } = await supabase.auth.getUser();
 
-  if (!user || error) {
-    return redirect("/sign-in");
-  }
+  // if (!user || error) {
+  //   return redirect("/sign-in");
+  // }
 
-  console.log(user);
+  // console.log(user);
 
   return (
     <AppLayout>
